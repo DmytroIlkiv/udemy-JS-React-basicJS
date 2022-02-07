0a2a0f2df4f0297ce93589383d60a23cf5d5e599
IlkivDmytro <ilkivdmytro2000@gmail.com>
2022-02-07 17:00:27 +0200

    first trial commit

---
+++ lesson one/js/script.js
@@ -0,0 +1,82 @@
+// "use strict";
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log("string"*2);
+
+// const person = "Alex "; 
+
+// const bool = true;
+
+// console.log(something); //null
+
+// let und;
+// console.log(und);
+
+// const obj = {
+// 	name: "Ivan",
+// 	age: 25,
+// 	isMarried: false,
+// };
+
+// console.log(obj.isMarried);
+// console.log(obj["name"]);
+
+// let arr = [12, "plum.png", "prange.jpg", "apple.bmp", {}, []];
+// console.log(arr[4]);
+
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are you older than 18?", "18");
+// console.log( answer + 23 );
+
+// const answers = [];
+
+// answers[0] = prompt("What is your name?", '');
+// answers[1] = prompt("What is your surname?", '');
+// answers[2] = prompt("How old are you?", '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);
+// const user = "Ivan";
+// alert(`Hello, ${user}`);
+
+// console.log(4 + /*+*/parseInt("5"));
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+
+// console.log(2 + 2 * 4 === "8");
+
+// // &&
+// // ||
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && isClose && true);
+// console.log(!isChecked || isClose);
+
+// console.log(2 + 2 * 2 !== '6');
+
+
+
+
+
+
